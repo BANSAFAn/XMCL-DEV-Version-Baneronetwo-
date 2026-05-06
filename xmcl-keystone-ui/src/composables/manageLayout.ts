20aca14eb501543a8ac1c829714d2817b30be086
ci010 <cijhn@hotmail.com>
2026-05-06 11:15:32 +0800

    refactor: Remove unused files

--- xmcl-keystone-ui/src/composables/manageLayout.ts
+++
@@ -1,6 +0,0 @@
-import { useLocalStorageCacheStringValue } from './cache'
-
-export function useManageLayout() {
-  const manageLayout = useLocalStorageCacheStringValue('manage_layout', 'classic' as 'classic' | 'modern')
-  return manageLayout
-}
